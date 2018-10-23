6be553d87336878c128ec2fcb2068c23a6f52a67
pmorski <morski.przemek@gmail.com>
2018-10-23 22:41:43 +0200

    blocked boardMiniatureEpic from launching when user is null

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -3,13 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const buildGravatarURL_1 = require("./buildGravatarURL");
 const admin = require("firebase-admin");
-exports.getGravatarUrl = functions.https.onCall((email) => {
-    return buildGravatarURL_1.buildGravatarUrl(email);
-});
 exports.getBoardMiniatures = functions.https.onCall((uid) => {
+    console.log('getBoardsMiniatures');
     const boardsCollection = admin.firestore().collection("boards");
     const field = "ownerId";
     const operator = "==";
-    return boardsCollection.where(field, operator, uid);
+    const response = boardsCollection.where(field, operator, uid);
+    console.log('getBoardsMiniatures' + response.toString());
+    return response;
+});
+exports.getGravatarUrl = functions.https.onCall((email) => {
+    return buildGravatarURL_1.buildGravatarUrl(email);
 });
 //# sourceMappingURL=index.js.map
--- src/epics/boardMiniaturesEpic.ts
+++ src/epics/boardMiniaturesEpic.ts
@@ -2,7 +2,7 @@ import * as firebase from "firebase";
 import {Action} from "redux";
 import {ActionsObservable, Epic, ofType, StateObservable} from "redux-observable";
 import {from, Observable} from "rxjs";
-import {map, mergeMap} from "rxjs/operators";
+import {filter, map, mergeMap} from "rxjs/operators";
 import {boardMiniatures} from "../actions/boardActions";
 import {USER_DATA} from "../actions/constants";
 import {IBoardMiniature} from "../entities/IBoard";
@@ -12,19 +12,17 @@ export const boardMiniaturesEpic: Epic<Action, Action, IState> = (
   action$: ActionsObservable<Action>,
   state$: StateObservable<IState>,
 ): Observable<Action> => {
-  debugger;
   return action$.pipe(
     ofType(USER_DATA),
-    mergeMap(() =>
-      from(firebase.functions().httpsCallable("getBoardMiniatures")(state$.value.user!.uid)).pipe(
-        map((response) => {
+    filter(() => state$.value.user !== null),
+    mergeMap(() => {
+      debugger;
+      return from(firebase.functions().httpsCallable("getBoardMiniatures")(state$.value.user!.uid)).pipe(
+        map((response: any) => {
           debugger;
-          return response.data.then((boards: IBoardMiniature[]) => {
-            debugger;
-            return boardMiniatures(boards);
-          });
+          return response.data.then((boards: IBoardMiniature[]) => boardMiniatures(boards));
         }),
-      ),
-    ),
+      );
+    }),
   );
 };
